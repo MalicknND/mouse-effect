a440573fb53c618dd14ea032c2d0d03d9d082e4b
Malick NDIAYE <98133571+MalicknND@users.noreply.github.com>
2023-08-07 17:17:59 +0200

    done

--- index.js
+++ index.js
@@ -6,8 +6,26 @@
 
 // Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
 
+// Premiere methode 
+
+// window.addEventListener('mousemove', (e) => {
+// cursor.style.top = e.y + 'px';
+// cursor.style.left = e.x + 'px';
+
+// mouse1.style.top = e.y + 'px';
+// mouse1.style.left = e.x + 'px';
+
+// mouse2.style.top = e.y + 'px';
+// mouse2.style.left = e.x + 'px';
+
+// })
+
+// Reduction du code avec forEach
+const mouses = document.querySelectorAll('.mouse')
 window.addEventListener('mousemove', (e) => {
-cursor.style.top = e.y + 'px';
-cursor.style.left = e.x + 'px';
+    mouses.forEach((mouse) => {
+        mouse.style.top = e.y + "px"
+        mouse.style.left = e.x + "px"
+    })
 })
 
